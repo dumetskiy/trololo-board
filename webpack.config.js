c080d06d2f824696610f92b6fa2043b561d3ef30
Unknown <dumetskiy@gmail.com>
2018-06-30 22:36:48 +0300

    Webpack installation

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+var webpack = require('webpack');
+
+module.exports = {
+    entry: ['./assets/js/index.js'],
+    output: {
+        path: __dirname + '/assets/js',
+        publicPath: '/assets/js',
+        filename: 'trololo-bundle.js'
+    },
+    module:{
+        rules:[
+
+            {
+                test: /\.(png|jpeg|ttf|...)$/,
+                use: [
+                    { loader: 'url-loader', options: { limit: 8192 } }
+                ]
+            },
+            {
+                test:/\.(scss|sass)$/,
+                use:[
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].css',
+                            outputPath: '../styles/'
+                        }
+                    },
+                    {
+                        loader: 'extract-loader'
+                    },
+                    {
+                        loader: 'css-loader'
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: () => [require('autoprefixer')]
+                    }
+            },
+            {
+                loader: 'sass-loader'
+            }
+
+        ],
+        exclude: /node_modules/
+    }
+],
+}
+
+};
